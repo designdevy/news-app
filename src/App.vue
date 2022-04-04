75507a7441e686848b3bc7357dc143d15f385a57
Muhammad Athar <muhammad.athar@outlook.co.th>
2022-04-04 15:52:03 +0700

    update app bar color

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="deep-purple accent-4" dark>
       <!-- Add News App Title -->
       <div>ThisWeek</div>
       <!-- <div class="d-flex align-center">
@@ -22,13 +22,6 @@
           width="100"
         />
       </div> -->
-
-      <!-- <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn> -->
     </v-app-bar>
 
     <v-main>
