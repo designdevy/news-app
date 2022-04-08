33ce73f3a4fb0521b3e8e17d280e6b0539b7c077
Muhammad Athar <muhammad.athar@outlook.co.th>
2022-04-09 00:16:03 +0700

    refactor NewsArticle

--- src/views/NewsArticle.vue
+++ src/views/NewsArticle.vue
@@ -1,15 +1,17 @@
 <template>
   <v-container class="py-10">
     <v-row class="d-flex flex-column">
-      <v-column class="mx-auto">
+      <v-col class="mx-auto">
         <v-img max-width="1000px" :src="this.articles[articleId - 1].urlToImage"></v-img>
-      </v-column>
-      <v-column class="mx-auto mt-10">
+      </v-col>
+      <v-col class="mx-auto mt-10">
         {{ this.articles[articleId - 1].description }}
-      </v-column>
-      <v-column class="mx-auto mt-10">
-        <router-link class="text-decoration-none" to="/">Back to Headlines</router-link>
-      </v-column>
+      </v-col>
+      <v-col class="mx-auto mt-10">
+        <router-link class="text-decoration-none" :to="{ name: 'home' }"
+          >Back to Headlines</router-link
+        >
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -20,19 +22,12 @@ import data from '../assets/data.json';
 export default {
   name: 'NewsArticle',
   props: {
-    articleId: {
-      type: Number,
-    },
+    articleId: null,
   },
   data() {
     return {
       articles: data.articles,
     };
-  },
-  computed: {
-    imageUrl() {
-      return 'this.articles[1]';
-    },
   },
 };
 </script>
