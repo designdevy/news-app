efb1667f20c11a7114d56252269651910f408b88
Muhammad Athar <muhammad.athar@outlook.co.th>
2022-04-09 00:57:01 +0700

    add fallback text when content is missing

--- src/views/NewsArticle.vue
+++ src/views/NewsArticle.vue
@@ -2,10 +2,10 @@
   <v-container class="py-10">
     <v-row class="d-flex flex-column">
       <v-col class="d-flex justify-center">
-        <v-img max-width="1000px" :src="this.articles[articleId - 1].urlToImage"></v-img>
+        <v-img max-width="1000px" :src="articles[articleId - 1].urlToImage"></v-img>
       </v-col>
       <v-col class="mx-auto mt-10 text-center" sm="6">
-        {{ this.articles[articleId - 1].description }}
+        {{ newsContent }}
       </v-col>
       <v-col class="mt-10 d-flex justify-center">
         <router-link class="text-decoration-none" :to="{ name: 'home' }"
@@ -28,6 +28,20 @@ export default {
     return {
       articles: data.articles,
     };
+  },
+  computed: {
+    description() {
+      return this.articles[this.articleId - 1].description;
+    },
+    newsTitle() {
+      return this.articles[this.articleId - 1].title.substr(
+        0,
+        this.articles[this.articleId - 1].title.indexOf('-') - 1,
+      );
+    },
+    newsContent() {
+      return this.description.length > 0 ? this.description : this.newsTitle;
+    },
   },
 };
 </script>
